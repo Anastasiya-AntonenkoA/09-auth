6d08940475d97ceb75461305ba485f665ef1e301
anastasiia <Nastyaantonenkoa@gmail.com>
2025-10-22 22:08:23 +0300

    fix fetchNoteById

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -3,7 +3,7 @@
 import css from "./NotePreview.module.css";
 import Modal from "@/components/Modal/Modal";
 import { useParams } from "next/navigation";
-import { fetchNoteById } from "@/lib/api/api";
+import { fetchNoteById } from "@/lib/api/clientApi";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
